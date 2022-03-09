e9bd36c089321bc9ec976c335646282c0c78b0d2
Andrey Kolobov <akolobov@microsoft.com>
2022-03-09 02:59:15 -0800

    Added a data writer

---
+++ metaworld/data/dataset.py
@@ -0,0 +1,83 @@
+import h5py
+import pickle
+import numpy as np
+
+SUBGOAL_BREAKDOWN = {
+    'assembly-v2' : ['grasp_success', 'lift_success', 'align_success']
+}
+
+class MWDatasetWriter:
+    def __init__(self, fname, env, task_name, res, camera, success_steps_for_termination):
+        # The number of steps with with info/success = True required to trigger episode termination
+        self.task_name = task_name
+        self.success_steps_for_termination = success_steps_for_termination
+        raw_metadata = {
+            'task_name' : task_name,
+            'horizon' : env.max_path_length,
+            'fps': env.metadata["video.frames_per_second"],
+            'frame_skip' : env.frame_skip,
+            'img_width' : res[0],
+            'img_height' : res[1],
+            'img_format' : 'cwh',
+            'camera' : camera,
+            'subgoal_breakdown' : SUBGOAL_BREAKDOWN[task_name], 
+            'success_steps_for_termination' : success_steps_for_termination
+        }
+
+        self.metadata = np.void(pickle.dumps(raw_metadata))
+        self._datafile = h5py.File(fname, 'w')
+        self._datafile.create_dataset("env_metadata", data=self.metadata)
+
+        self.data = self._reset_data()
+        self._num_episodes = 0
+        
+
+    def _reset_data(self):
+        data = {
+            'states': [],
+            'observations': [],
+            'actions': [],
+            'terminals': [],
+            'rewards': [],
+            'infos/goal': []
+            }
+        
+        for subgoal in SUBGOAL_BREAKDOWN[self.task_name]:
+            data['infos/' + subgoal] = []
+        
+        return data
+
+
+    def append_data(self, s, o, a, r, done, info):
+        self.data['states'].append(s)
+        #self.data['observations'].append(np.asarray(o))
+        self.data['observations'].append(o)
+        self.data['actions'].append(a)
+        self.data['rewards'].append(r)
+        self.data['terminals'].append(done)
+        self.data['infos/goal'].append(info['success'])
+
+        for subgoal in SUBGOAL_BREAKDOWN[self.task_name]:
+            self.data['infos/' + subgoal].append(float(info[subgoal]))
+
+
+    def write_trajectory(self, max_size=None, compression='gzip'):
+        np_data = {}
+        for k in self.data:
+            if k == 'terminals':
+                dtype = np.bool_
+            else:
+                dtype = np.float32
+            data = np.array(self.data[k], dtype=dtype)
+            if max_size is not None:
+                data = data[:max_size]
+            np_data[k] = data
+
+        trajectory = self._datafile.create_group('traj_' + str(self._num_episodes))
+        self._num_episodes += 1
+
+        for k in np_data:
+            trajectory.create_dataset(k, data=np_data[k], compression=compression)
+
+    def close(self):
+        self._datafile.close()
--- metaworld/data/training_data_gen.py
+++ metaworld/data/training_data_gen.py
@@ -6,12 +6,14 @@ from metaworld.envs import (ALL_V2_ENVIRONMENTS_GOAL_OBSERVABLE,
                             ALL_V2_ENVIRONMENTS_GOAL_HIDDEN)
 from metaworld.policies import *
 from tests.metaworld.envs.mujoco.sawyer_xyz.test_scripted_policies import test_cases_latest_nonoise
+from metaworld.data.dataset import *
 import gym
+
 # Suppress float conversion warnings 
 gym.logger.set_level(40)
 
 
-# TODO: This doesn't work! How do we seed single goal envs?
+# TODO: This doesn't work. How do we seed single goal envs?
 """
 SEED = 0  # some seed number here
 benchmark = metaworld.BENCHMARK(seed=SEED)
@@ -40,29 +42,34 @@ def writer_for(tag, fps, res):
         res
     )
 
-# TODO: WHICH ACTION NOISE LEVEL TO CHOOSE? HOW TO SET IT? FOR HINTS, SEE https://github.com/rlworkgroup/metaworld/blob/cfd837e31d65c9d2b62b7240c68a26b04a9166d9/tests/metaworld/envs/mujoco/sawyer_xyz/test_scripted_policies.py
+# TODO: which action noise level to apply? For ideas, see https://github.com/rlworkgroup/metaworld/blob/cfd837e31d65c9d2b62b7240c68a26b04a9166d9/tests/metaworld/envs/mujoco/sawyer_xyz/test_scripted_policies.py
 def config_env(e):
     e._partially_observable = False
     e._freeze_rand_vec = False
     e._set_task_called = True
 
 
-res = (640, 480)
+res = (84, 84)
 camera = 'corner' # one of ['corner', 'topview', 'behindGripper', 'gripperPOV']
+MAX_steps_at_goal = 10
+
 #print(metaworld.ML1.ENV_NAMES)  # Check out the available environments
+
+target_tasks = ['assembly-v2']
 
 for case in test_cases_latest_nonoise:
     
-    
-    if case[0] != 'assembly-v2':
+    if case[0] not in target_tasks: #!= 'assembly-v2':
         continue
-
-
-    #task_name = 'pick-place-v2'
+    
+    """
     if case[0][-3:] != '-v2':
         continue
+    """
 
     task_name = case[0]
+    policy = case[1]
+
     print(f'----------Running task {task_name}------------')
 
     """
@@ -76,48 +83,44 @@ for case in test_cases_latest_nonoise:
     env = ml1.train_classes[task_name]()  # Create a **training** goal distribution environment
     task = random.choice(ml1.train_tasks)
     env.set_task(task)  # Set task
-
-    print(f'Generating a video at {env.metadata["video.frames_per_second"]} fps')
-    
     config_env(env)
-    policy = case[1]
-
-    """
-    policy = None
-
-    for case in test_cases_latest_nonoise:
-        if case[0] == task_name:
-            print(f'Found a scripted policy for {task_name}!')
-            policy = case[1]
-            break
-    """
-
-    #print(f'Problem horizon: {env.max_path_length}')
-
     num_successes = 0
-    num_attemps = 1 #10
+    num_attemps = 2 
+
+    data_writer = MWDatasetWriter(task_name + '.h5py', env, task_name, res, camera, MAX_steps_at_goal)
 
     for attempt in range(num_attemps):
-        #writer = writer_for(task_name + '/' + task_name + '-' + str(attempt), env.metadata['video.frames_per_second'], res)
+        print(f'Generating a video at {env.metadata["video.frames_per_second"]} fps')
         writer = writer_for(task_name + '-' + str(attempt + 1), env.metadata['video.frames_per_second'], res)
-        state = env.reset()  # Reset environment
+        state = env.reset()
+        obs = env.sim.render(*res, mode='offscreen', camera_name=camera)[:,:,::-1]
+        writer.write(obs)
         success_recorded =  False
         
         t = 0
         for t in range(env.max_path_length):
-            a = policy.get_action(state) #env.action_space.sample()  # Sample an action
-            state, reward, done, info = env.step(a)  # Step the environoment with the sampled random action
-            print(f"Step {t} |||| near-object-rew: {info['near_object']}, grasp-rew: {info['grasp_reward']}, grasp-succ: {info['grasp_success']}, in-place-rew: {info['in_place_reward']}, obj-to-target-rew: {info['obj_to_target']}")
+            action = policy.get_action(state)
+            new_state, reward, done, info = env.step(action)
+            data_writer.append_data(state, obs, action, reward, done, info) #self, s, o, a, r, done, info)
+            #print(f"Step {t} |||| near-object-rew: {info['near_object']}, grasp-rew: {info['grasp_reward']}, grasp-succ: {info['grasp_success']}, lift-succ: {info['lift_success']}, align-succ: {info['align_success']}, in-place-rew: {info['in_place_reward']}, obj-to-target-rew: {info['obj_to_target']}, success: {info['success']}")
+            state = new_state
             obs = env.sim.render(*res, mode='offscreen', camera_name=camera)[:,:,::-1]
             writer.write(obs)
-            # TODO: record fixed-length trajectories? Or only until success? 
             
-            if info['success'] and not success_recorded:
+            # TODO: record fixed-length trajectories? Or only until success? 
+
+            if info['success'] and steps_at_goal >= MAX_steps_at_goal:
                 print(f'Attempt {attempt} succeeded at step {t}')
                 num_successes += 1
                 success_recorded = True
-                #break
+                break
+            elif info['success']:
+                steps_at_goal += 1
+            else:
+                steps_at_goal = 0
 
+        data_writer.write_trajectory()
         print(f'Episode ended at time step {t}')
 
+    data_writer.close()
     print(f'Success rate for {task_name}: {num_successes / num_attemps}\n')
